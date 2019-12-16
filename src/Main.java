7b45b2d89c69144ec40fa52a645453fac3a0b53a
yuval <yuval.cole@gmail.com>
2019-12-16 12:21:46 +0100

    Solved bugs

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,21 @@ public class Main {
 
     public static void main(String[] args) {
         String emailAddress = "john.doe.mis2016@ie.edu";
-        String formattedEmailAddress = emailAddress.replaceAll(".", "/");
+        String formattedEmailAddress = emailAddress.replaceAll("\\.", "/");
 
         Random random = new Random();
-        StringBuffer word = null;
+        StringBuffer word = new StringBuffer(); //bug 1 bbeofre was rod = null instead of = new StringBuffer()
 
-        switch (random.nextInt(2)) {
+        switch (random.nextInt(3)) {
             case 0:
-                word = new StringBuffer('Y');
+                word = word.append("Y");            //bug 2 .append
+                break;
             case 1:
-                word = new StringBuffer('F');
+                word = word.append("F");            //bug 3 Equiprobability, random had to be valued to 3.
+                break;
             case 2:
-                word = new StringBuffer('T');
+                word = word.append("T");
+                break;
         }
 
         word.append('o');
